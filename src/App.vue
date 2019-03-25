32052cd91175f059374ab2d8b710f9bf21fc54b3
ledinhhuan <ledinhhuan205@gmail.com>
2019-03-25 11:52:22 +0700

    Add and delete quote

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">
+          Info: Click on a Quote to delete it!
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Header from './components/Header.vue';
+  import NewQuote from './components/NewQuote.vue';
+  import QuoteGrid from './components/QuoteGrid.vue';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      appHeader: Header,
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote,
+    },
+    data() {
+      return {
+        quotes: [
+          'Just a quote',
+        ],
+        maxQuotes: 10,
+      }
+    },
+    methods: {
+      newQuote(quote) {
+        if(this.quotes.length >= this.maxQuotes) {
+          return alert("Please delete quote!")
+        }
+        this.quotes.push(quote)
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index, 1)
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
